cc15e9b6855c721a502ff2be4a42ba717c3c22bc
dude5466 <lordsparda33@gmail.com>
2025-06-15 00:39:54 +0300

    Update w.js

--- w.js
+++ w.js
@@ -2,8 +2,8 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/service-worker.js')
-      .then(reg => console.log('✅ Service Worker registered', reg))
-      .catch(err => console.error('❌ Service Worker registration failed:', err));
+      .then(reg => console.log(' Service Worker registered', reg))
+      .catch(err => console.error(' Service Worker registration failed:', err));
   });
 }
 
